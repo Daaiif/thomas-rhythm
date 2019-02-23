1ce96d6a19b1cf05d92454090fd0a556d33bd1d2
Mahmut <grupry@gmail.com>
2019-02-23 23:32:47 +0200

    added reviews section with slide

--- js/init.js
+++ js/init.js
@@ -2,4 +2,9 @@ $( function() {
   // Init tabs
     $( "#tabs" ).tabs();
     $( "#tabs-portfolio" ).tabs();
+
+    // init slick carousel
+    $('.reviews-carousel').slick({
+      dots: true,
+    });
 });
